71d3ffa3a0074c5edb3cd8577b60b00d2e37c68c
John Peet <john@cyber-duck.co.uk>
2016-11-13 05:27:33 +0000

    Animation loop

--- bubbles.js
+++ bubbles.js
@@ -1,13 +1,13 @@
 module.exports = {
     bubbles: [
-        { width: 20, height: 20, x: "20%", duration: 10, delay: 6 },
-        { width: 28, height: 28, x: "4%", duration: 8, delay: 8 },
-        { width: 12, height: 12, x: "9%", duration: 9, delay: 4 },
-        { width: 18, height: 18, x: "72%", duration: 7, delay: 0 },
-        { width: 20, height: 20, x: "44%", duration: 10, delay: 12 },
-        { width: 38, height: 38, x: "80%", duration: 12, delay: 14 },
-        { width: 12, height: 12, x: "88%", duration: 16, delay: 2 },
-        { width: 18, height: 18, x: "34%", duration: 18, delay: 3 }
+        { width: 20, height: 20, x: "20%", frequency: 0.06, y: 216 },
+        { width: 28, height: 28, x: "4%", frequency: 0.12, y: 228 },
+        { width: 12, height: 12, x: "9%", frequency: 0.13, y: 204 },
+        { width: 18, height: 18, x: "72%", frequency: 0.08, y: 200 },
+        { width: 20, height: 20, x: "44%", frequency: 0.045, y: 212 },
+        { width: 38, height: 38, x: "80%", frequency: 0.04, y: 214 },
+        { width: 12, height: 12, x: "88%", frequency: 0.06, y: 220 },
+        { width: 18, height: 18, x: "34%", frequency: 0.08, y: 230 }
     ],
 
     domains: [
